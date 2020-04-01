5a6ebefc7034482f08da3825dfd3cb6a69f10746
Daniele Barsotti <sixtynine.db@gmail.com>
2020-04-01 17:12:12 +0200

    Add led to the arduino sketch

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -10,6 +10,61 @@ LightResult lightRes;
 DhtResult dhtRes;
 DallasResult dallasRes;
 
+void outputLight()
+{
+    Serial.print(lightRes.reading);
+    Serial.print("/");
+    Serial.print(lightRes.voltage);
+    Serial.print("/");
+    Serial.print(lightRes.resistance);
+    Serial.print("/");
+    Serial.println(lightRes.intensity);
+}
+
+void outputDHT()
+{
+    Serial.print(dhtRes.temperature);
+    Serial.print("/");
+    Serial.print(dhtRes.humidity);
+    Serial.print("/");
+    Serial.println(dhtRes.heatIndex);
+}
+
+void outputDallas()
+{
+    Serial.print(dallasRes.temperature);
+    Serial.print("/");
+    Serial.print(dallasRes.resolution);
+    Serial.print("/");
+    Serial.println(dallasRes.parasitePower);
+}
+
+unsigned long ledInterval = 100;
+bool ledBlinking = false;
+bool ledOn = false;
+
+Timer t = Timer(UPDATE_DELAY, updateSensors);
+Timer t1 = Timer(ledInterval, updateLed);
+static const int bufferSize = 30;
+
+void updateLed() {
+  static int count = 0;
+
+  if (!ledBlinking) {
+    digitalWrite(LED_PIN, HIGH);
+    return;
+  }
+
+  ledOn = !ledOn;
+  digitalWrite(LED_PIN, (ledOn ? HIGH : LOW));
+  count++;
+
+  if (count > 5) {
+    ledBlinking = false;
+    count = 0;
+  }
+}
+
 void updateSensors()
 {
     light.update();
@@ -20,64 +75,43 @@ void updateSensors()
     dallasRes = dallas.read();
 }
 
-void outputLight()
-{
-    Serial.print(lightRes.reading);
-    Serial.print("/");
-    Serial.print(lightRes.voltage);
-    Serial.print("/");
-    Serial.print(lightRes.resistance);
-    Serial.print("/");
-    Serial.print(lightRes.intensity);
-}
-
-void outputDHT()
-{
-    Serial.print(dhtRes.temperature);
-    Serial.print("/");
-    Serial.print(dhtRes.humidity);
-    Serial.print("/");
-    Serial.print(dhtRes.heatIndex);
-}
-
-void outputDallas()
-{
-    Serial.print(dallasRes.temperature);
-    Serial.print("/");
-    Serial.print(dallasRes.resolution);
-    Serial.print("/");
-    Serial.print(dallasRes.parasitePower);
-}
-
-Timer t = Timer(UPDATE_DELAY, updateSensors);
-static const int bufferSize = 30;
-
 void serialEvent()
 {
     char buffer[bufferSize];
     int sizeRead = Serial.readBytesUntil('\n', buffer, 1);
 
     if (bufferSize > 0) {
+
+        ledBlinking = true;
+        delay(0);
+
         switch (buffer[0]) {
-            case '1': return outputLight();
-            case '2': return outputDHT();
-            case '3': return outputDallas();
+            case '1':
+                outputLight();
+                break;
+            case '2':
+                outputDHT();
+                break;
+            case '3':
+                outputDallas();
+                break;
+            case '0':
+              outputLight();
+              outputDHT();
+              outputDallas();
+              break;
         }
     }
-//     Serial.print("Received: ");
-//     Serial.print(bufferSize);
-//     Serial.print(" bytes: / ");
-//     for (int i = 0 ; i < sizeRead ; i++) {
-//         Serial.print(buffer[i], HEX);
-//         Serial.print(" / ");
-//     }
-//     Serial.println();
 }
 
 void setup()
 {
-    Serial.begin(9600);
     pinMode(LIGHT_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
+
+    Serial.begin(9600);
+
     light.start();
     dht.start();
     dallas.start();
@@ -86,4 +120,5 @@ void setup()
 void loop()
 {
     t.update();
+    t1.update();
 }
--- arduino/constants.h
+++ arduino/constants.h
@@ -7,6 +7,8 @@
 #define ONE_WIRE_BUS 3
 #define DALLAS_PRECISION 9
 
+#define LED_PIN 4
+
 #define LIGHT_PIN A1
 #define LIGHT_RESISTOR 10000
 
